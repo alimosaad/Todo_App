a8e045e3332546f620664f8718be70e229e93db7
ali mosaad <alimosaad573@gmail.com>
2022-12-24 13:31:43 +0200

    add screens components

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,14 +1,60 @@
 import 'package:flutter/material.dart';
 
-class HomeLayout extends StatelessWidget {
+import '../screens/settings/settings.dart';
+import '../screens/tasks/tasks_list.dart';
+
+class HomeLayout extends StatefulWidget {
   static const String routeName = 'Home';
+
+  @override
+  State<HomeLayout> createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  int currentIndex=0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,     //to make notch with the same color of body
       appBar: AppBar(
         title: Text("To Do List"),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+      floatingActionButton: FloatingActionButton(
+        shape: StadiumBorder(
+          side: BorderSide(
+            color: Colors.white,
+            width: 3,
+          )
+        ),
+        onPressed: (){},
+        child: Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 7,
+        shape: CircularNotchedRectangle(),
+
+        child: BottomNavigationBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          currentIndex:currentIndex ,
+          onTap: (index){
+            currentIndex=index;
+            setState(() {
+
+            });
+          },
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.list,size: 30,),label: 'Lists'),
+            BottomNavigationBarItem(icon: Icon(Icons.settings,size: 30,),label: 'Settins'),
+          ],
+        ),
+      ),
+      body:tabs[currentIndex] ,
     );
   }
+
+  List<Widget>tabs=[TasksList(),SettingsScreen()];
 }
